e7c1eb13273b1c953c87da2211d274b6f56fa090
Oleksii Ziuzin <ziuzinpl@gmail.com>
2025-03-23 10:13:50 +0100

    Update asset paths in index.html and enhance Vite configuration for improved build process

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
   base: '/goit-react-hw-01/',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   build: {
     sourcemap: true,
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
   },
 });
