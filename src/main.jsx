5af991fe0c55a170f628f3e76f5b46a5bace69d5
Oleksii Ziuzin <ziuzinpl@gmail.com>
2025-03-23 01:23:37 +0100

    Fix formatting in main.jsx by adding a trailing space after the index.css import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'modern-normalize/modern-normalize.css';
-import './index.css';
+import './index.css'; 
 import App from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
