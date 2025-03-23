337f0439d4d8fe2f04c0c9bb71077b1c257a585b
Oleksii Ziuzin <ziuzinpl@gmail.com>
2025-03-23 01:17:09 +0100

    Update script source path in index.html and enhance asset file naming in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,16 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: undefined,
-        assetFileNames: 'assets/[name].[ext]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
-      }
-    }
-  }
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.')[1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
+  },
 });
